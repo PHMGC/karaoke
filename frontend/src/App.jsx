b183c32e64179a63d91c6aed4ff1e01a03a043eb
PHMGC <108164511+PHMGC@users.noreply.github.com>
2024-10-17 15:47:45 -0300

    modelo base de um site flask-react

---
+++ frontend/src/App.jsx
@@ -0,0 +1,27 @@
+import { useState, useEffect } from 'react'
+import ContactList from './ContactList'
+import './App.css'
+import ContactForm from './ContactForm'
+
+function App() {
+
+    const [contacts, setContacts] = useState([])
+
+    useEffect(() => {
+        fetchContacts()
+    }, [])
+
+    const fetchContacts = async () => {
+        const reponse = await fetch("http://127.0.0.1:5000/contacts")
+        const data = await reponse.json()
+        setContacts(data.contacts)
+        console.log(data.contacts)
+    }
+
+    return (<>
+        <ContactList contacts={contacts} />
+        <ContactForm />
+    </>)
+}
+
+export default App
